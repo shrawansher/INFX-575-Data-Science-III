03fc54248eed7ee2701ec983a641797307bf42bf
Shrawan Sher <shrawansher@gmail.com>
2017-10-08 10:50:16 -0700

    Q4

---
+++ assignment01_twitter/frequency.py
@@ -0,0 +1,32 @@
+import sys
+import json
+
+def main():
+    tweet_file = open(sys.argv[1])
+    word_freq ={}
+    total_words =0
+
+    for line in tweet_file:
+        tweet = json.loads(line)
+        if 'text' in tweet:
+            words = tweet['text'].lower().split(" ")
+            for word in words:
+                if word in word_freq:
+
+                    word_freq[word] = word_freq[word]+1
+                else:
+                    word_freq[word]=1
+
+                print("*******", word_freq[word])
+
+                total_words +=1
+
+
+    for t in word_freq:
+        print(t, " ", word_freq[t]/total_words)
+
+
+
+
+if __name__ == '__main__':
+    main()

799bcd0a4a30ae24f427c5a7caaf5ddaccce4168
Shrawan Sher <shrawansher@gmail.com>
2017-10-08 09:16:11 -0700

    Q1,2,3

--- assignment01_twitter/term_sentiment.py
+++ assignment01_twitter/term_sentiment.py
@@ -1,17 +1,77 @@
 import sys
+import json
+import re
 
-def hw():
-    print 'Hello, world!'
 
-def lines(fp):
-    print str(len(fp.readlines()))
+def read_scores(sent_file):
+    with open(sent_file) as f:
+        return {line.split('\t')[0]: int(line.split('\t')[1]) for line in f}
+
+
+def score_tweet(tweet, scores):
+    return sum(scores.get(word, 0) for word in tweet)
 
 def main():
     sent_file = open(sys.argv[1])
+
     tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+
+    afinn_scores = {}
+    tweet_scores={}
+    new_words=[]
+    tweets = []
+
+
+
+    for line in sent_file:
+        word, score = line.strip().split("\t")
+        afinn_scores[word] = score
+
+    #Store the new tweets
+    for line in tweet_file:
+        tweet = json.loads(line)
+        if 'text' in tweet:
+            text = tweet['text'].lower()
+            tweets.append(text)
+
+    #Calculate scores
+
+    for tweet in tweets:
+        #print(tweet)
+        score = 0
+        words = tweet.split(" ")
+
+        for word in words:
+            if word in afinn_scores:
+                score += int(afinn_scores[word])
+
+            else:
+                new_words.append(word)
+            tweet_scores[tweet] = score
+
+    #Calculate the scores for the new words
+
+    for word in new_words:
+        pos =0
+        neg =0
+        total=0
+        for tweet in tweet_scores:
+            if word in tweet:
+                if tweet_scores[tweet]>0:
+                    pos+=1
+                elif tweet_scores[tweet]<0:
+                    neg+=1
+
+                total+=1
+
+        print(word,":",(pos-neg)/total)
+
+
+
+
+
+
+
 
 if __name__ == '__main__':
     main()
--- assignment01_twitter/tweet_sentiment.py
+++ assignment01_twitter/tweet_sentiment.py
@@ -1,17 +1,70 @@
 import sys
+import json
+import re
 
 def hw():
-    print 'Hello, world!'
+    print('Hello, world!')
 
 def lines(fp):
-    print str(len(fp.readlines()))
+    print(str(len(fp.readlines())))
+
+def calculate_score(words,afinn_scores):
+    score =0
+    for word in words:
+        if word in afinn_scores:
+            score += afinn_scores[word]
+
+    return score
+
+
+def extract_words(tweet):
+    words = tweet.split(" ")
+    return [ word.lower() for word in words if len(word)>1]
 
 def main():
     sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
 
+    tweet_file = open(sys.argv[2])
+    #tweet_json = json.loads(tweet_file)
+    #print(tweet_json)
+    #print(tweet_file)
+    #tweet_file = json.loads(tweet_file.decode("utf-8"))
+
+    afinn_scores = {}  # initialize an empty dictionary
+    for line in sent_file:
+        term, score = line.strip().split("\t")  # The file is tab-delimited. "\t" means "tab character"
+        afinn_scores[term] = int(score)  # Convert the score to an integer.
+    sentiment_score =[]
+    #tweet_sentiments = {}
+
+    for line in tweet_file:
+        print(type(line))
+        tweet_line = json.loads(line)
+        score = 0
+        #Fetch tweet text
+        if "text" in tweet_line:
+            tweet = tweet_line["text"]
+            print(tweet)
+            #tweet = tweet.encode('ascii', 'ignore')
+            print(tweet)
+
+        #Extract words
+            words = extract_words(tweet)
+
+            score = calculate_score(words,afinn_scores)
+        sentiment_score.append(score)
+
+
+    print(sentiment_score)
+
+
+
+    
+    
 if __name__ == '__main__':
     main()
+   # afinnfile = open("AFINN-111.txt")
+
+   # tweet_file = open("output.txt")
+
+
